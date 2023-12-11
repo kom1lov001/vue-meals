3ad7304b2bdf4f2ca92376a7c862238efaa5af4e
kom1lov001 <avazkomilov94@gmail.com>
2023-12-11 18:08:08 +0300

    action commit

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,7 +1,7 @@
 import axiosClient from "../axiosClient.js";
 
 export function searchMeals({ commit }, keyword) {
-  axiosClient.get(`search.php?s=${keyword}`).then(({ data }) => {
+  axiosClient.get(`/search.php?s=${keyword}`).then(({ data }) => {
     commit("setSearchedMeals", data.meals);
   });
 }
